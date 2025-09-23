13ee0a4c79df7b62ca57dfb094a6a22acbd12b9d
AndreaBedoya <andreapaola.nino2005@gmail.com>
2025-09-23 16:39:04 -0500

    Estilos base de la landingpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,16 @@
-import { useEffect, useState } from "react";
-import UploadForm from "./components/UploadForm";
-import Swal from 'sweetalert2';
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./components/LandingPage";
+import UploadForm from "./components/UploadForm"; 
+import "./index.css";
 
 function App() {
-  const [msg, setMsg] = useState("Cargando...");
-
-  useEffect(() => {
-    fetch("http://localhost:4000/api/hello")
-      .then((res) => res.json())
-      .then((data) => setMsg(data.message))
-      .catch((err) => {
-        console.error("Error al conectar con el backend:", err);
-        setMsg("❌ No se pudo conectar con el backend");
-      });
-  }, []);
-
   return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>{msg}</h1>
-      <UploadForm />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/Upload" element={<UploadForm />} />
+      </Routes>
+    </Router>
   );
 }
 
